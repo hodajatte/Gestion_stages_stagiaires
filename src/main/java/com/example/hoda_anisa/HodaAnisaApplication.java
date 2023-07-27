484a06a42ff968f015f0f798c58a3d1d6fe48090
hodajatte <88599603+hodajatte@users.noreply.github.com>
2023-07-27 17:23:46 +0100

    Initial commit

---
+++ src/main/java/com/example/hoda_anisa/HodaAnisaApplication.java
@@ -0,0 +1,13 @@
+package com.example.hoda_anisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HodaAnisaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HodaAnisaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hoda_anisa/HodaAnisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hoda_anisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HodaAnisaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
